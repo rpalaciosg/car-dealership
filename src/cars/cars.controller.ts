aa2acd88fb18016cb354c29d9943daab8588a4ce
Richard Palacios <apalaciosg91@gmail.com>
2026-05-02 13:53:09 -0500

    feat: add car service and provider
    - Nest services
    - Nest providers
    - Independence inyection

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({ id: +id });
+    const carName = this.carsService.findById(Number(id));
+    return { carName };
   }
 }
